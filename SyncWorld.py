abb537bbdd2bdc276f22667ac8bf5e5dc98b97b8
Mihail Dobrev <mixaildobrev@gmail.com>
2020-07-01 23:31:37 +0300

    asd

--- SyncWorld.py
+++ SyncWorld.py
@@ -19,35 +19,48 @@ repo.remote("origin").repo.git.checkout("origin/master", "mcServerState.txt")
 f = open("mcServerState.txt", "r")
 state = f.readline()
 print("No server is running starting yours")
-if state == True:
-    print("Server is working")
+try:
+    if state == True:
+        print("Server is working")
+        exit()
+    f.close()
+
+    f = open("mcServerState.txt", "w")
+    f.write("True")
+    f.close()
+
+    repo.git.add("mcServerState.txt")
+    repo.index.commit(COMMIT_MESSAGE)
+    origin = repo.remote(name='origin')
+    origin.push()
+
+    repo.remotes.origin.pull()
+
+    os.chdir("McServer")
+
+    #Here is where the server starts
+    for output_line in run_command('java -Xmx1024M -Xms1024M -jar server.jar nogui'):
+        print(output_line)
+
+    os.chdir("../")
+except:
+    f = open("mcServerState.txt", "w")
+    f.write("False")
+    f.close()
+
+    repo.git.add(".")
+    repo.index.commit("World Save")
+    origin = repo.remote(name='origin')
+    origin.push()   
+
+    print("There was an error")
     exit()
-f.close()
-
-f = open("mcServerState.txt", "w")
-f.write("True")
-f.close()
-
-repo.git.add("mcServerState.txt")
-repo.index.commit(COMMIT_MESSAGE)
-origin = repo.remote(name='origin')
-origin.push()
-
-repo.remotes.origin.pull()
-
-os.chdir("McServer")
-
-#Here is where the server starts
-for output_line in run_command('java -Xmx1024M -Xms1024M -jar server.jar nogui'):
-    print(output_line)
-
-os.chdir("../")
 
 f = open("mcServerState.txt", "w")
 f.write("False")
 f.close()
 
-repo.git.add(".")
+repo.git.add("mcServerState.txt")
 repo.index.commit("World Save")
 origin = repo.remote(name='origin')
 origin.push()
