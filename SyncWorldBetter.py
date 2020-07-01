b96bdcf6fa8952f5202006b2e6d163931542ab1f
Mihail Dobrev <mixaildobrev@gmail.com>
2020-07-01 22:46:03 +0300

    asd

--- SyncWorldBetter.py
+++ SyncWorldBetter.py
@@ -15,7 +15,7 @@ def run_command(command):
 
 repo = Repo(PATH_OF_GIT_REPO)
 repo.remote().origin.repo.git.checkout("origin/master", "mcServerState.txt")
-
+print("asdaasdadasdasdas")
 f = file.open("mcServerState.txt", "r")
 state = f.readline()
 print(state)
@@ -23,21 +23,25 @@ if state == True
     print("Server is working")
     exit()
 f.close()
+print("asdaasdadasdasdas")
 
 f.open("mcServerState.txt", "w")
 f.write("True")
 f.close()
+print("asdaasdadasdasdas")
 
 repo.git.add("mcServerState.txt")
 repo.index.commit(COMMIT_MESSAGE)
 origin = repo.remote(name='origin')
 origin.push()
+print("asdaasdadasdasdas")
 
 repo.remotes.origin.pull()
+print("asdaasdadasdasdas")
 
-os.chdir("McServer")
-#Here is where the server starts
-for output_line in run_command('java -Xmx1024M -Xms1024M -jar server.jar nogui'):
-    print(output_line)
-os.chdir("../")
-print("done")
+# os.chdir("McServer")
+# #Here is where the server starts
+# for output_line in run_command('java -Xmx1024M -Xms1024M -jar server.jar nogui'):
+#     print(output_line)
+# os.chdir("../")
+# print("done")
