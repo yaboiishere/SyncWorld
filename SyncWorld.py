431fb03e2c899cd004a2add99c2c2c6d24eff8f7
Mihail Dobrev <mixaildobrev@gmail.com>
2020-07-01 22:57:33 +0300

    Final fix commit i hope

--- SyncWorld.py
+++ SyncWorld.py
@@ -5,17 +5,7 @@ import socket
 import subprocess
 
 PATH_OF_GIT_REPO = os.getcwd()
-COMMIT_MESSAGE = 'updated ip list'
-
-def ping(host, port):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(1)
-        if s.connect((host, port)):
-            s.close()
-    except:
-        return False
-    return True
+COMMIT_MESSAGE = 'updated state'
 
 def run_command(command):
     p = subprocess.Popen(command,
@@ -23,47 +13,33 @@ def run_command(command):
                      stderr=subprocess.STDOUT)
     return iter(p.stdout.readline, b'')
 
-ip = get('https://api.ipify.org').text
-
-# if os.path.isdir(PATH_OF_GIT_REPO+"\\.git") == False:
-#     repo = git.Repo.init("~/")
-# else:
-
 repo = Repo(PATH_OF_GIT_REPO)
-ips = []
-ipInListFlag = False
-with open("mcServerIps.txt", "r" ) as file:
-    for line in file:
-        currentLine = line[:-1]
-        if currentLine == ip:
-            ipInListFlag = True
-        ips.append(currentLine)
-    file.close()
+repo.remote("origin").repo.git.checkout("origin/master", "mcServerState.txt")
 
-if(not ipInListFlag):
-    file = open("mcServerIps.txt", "a")
-    file.write(ip + "\n")
-    repo = Repo(PATH_OF_GIT_REPO)
-    repo.git.add("mcServerIps.txt")
-    repo.index.commit(COMMIT_MESSAGE)
-    origin = repo.remote(name='origin')
-    origin.push()
-
-serverIsWorkingSomewhere = False
-serverIsWorkingOnIp = ""
-for i in ips:
-    if ping(i,25565):
-        serverIsWorkingSomewhere = True
-        serverIsWorkingOnIp = i + ":25565"
-
-
-if serverIsWorkingSomewhere:
-    print("Server is working on " + serverIsWorkingOnIp)
+f = open("mcServerState.txt", "r")
+state = f.readline()
+print(state)
+if state == True:
+    print("Server is working")
     exit()
+f.close()
+
+f = open("mcServerState.txt", "w")
+f.write("True")
+f.close()
+
+repo.git.add("mcServerState.txt")
+repo.index.commit(COMMIT_MESSAGE)
+origin = repo.remote(name='origin')
+origin.push()
+
 repo.remotes.origin.pull()
+
 os.chdir("McServer")
+
 #Here is where the server starts
-for output_line in run_command('java -Xmx1024M -Xms1024M -jar server.jar'):
+for output_line in run_command('java -Xmx1024M -Xms1024M -jar server.jar nogui'):
     print(output_line)
+
 os.chdir("../")
 print("done")
