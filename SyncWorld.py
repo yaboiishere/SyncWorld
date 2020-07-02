53b7861ca99e1abc0bc58e67c4152db3a14a443a
Mihail Dobrev <mixaildobrev@gmail.com>
2020-07-02 12:59:39 +0300

    documentation

--- SyncWorld.py
+++ SyncWorld.py
@@ -5,9 +5,12 @@ import socket
 import subprocess
 import time
 
+#git variables
 PATH_OF_GIT_REPO = os.getcwd()
 COMMIT_MESSAGE = 'updated state'
+repo = Repo(PATH_OF_GIT_REPO)
 
+#Just starts a command and returns the output
 def run_command(command):
     p = subprocess.Popen(command,
                      stdout=subprocess.PIPE,
@@ -15,35 +18,27 @@ def run_command(command):
                      shell=True)
     return iter(p.stdout.readline, b'')
 
-repo = Repo(PATH_OF_GIT_REPO)
-# repo.git.add("McServer/*")
-# repo.index.commit("World Save")
-# origin = repo.remote(name='origin')
-# origin.push()
-
+#Pulls from git repo because it won't push later
 repo.remotes.origin.pull()
-# time.sleep(20)
-# def getShitDone():
-#     repo.remote("origin").repo.git.checkout("origin/master", "mcServerState.txt")
-#     return True
 
-# if getShitDone():
-#     print("Got shit done")
-
+#Opens mcServerState.txt and gets the value
 f = open("mcServerState.txt", "r")
 state = f.readline()
 print(state)
 
 try:
+    #If the value is True it shuts down the script
     if state == "True":
         print("Server is working")
         exit()
     f.close()
 
+    #Updates the value
     f = open("mcServerState.txt", "w")
     f.write("True")
     f.close()
     
+    #Uploads the txt to Git
     repo.git.add("mcServerState.txt")
     repo.index.commit(COMMIT_MESSAGE)
     origin = repo.remote(name='origin')
@@ -51,15 +46,20 @@ try:
 
     print("No server is running starting yours")
 
+    #Changes the current working directory so the server unpacks in the proper folder
     os.chdir("McServer")
     print(os.getcwd())
+
     #Here is where the server starts
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
     my_file = os.path.join(THIS_FOLDER, 'server.jar')
     for output_line in run_command('java -Xmx1024M -Xms1024M -jar '+ my_file + " nogui"):
         print(output_line)
 
+    #Changes the current directory to parrent, so to be able to upload to git
     os.chdir("../")
+
+#If an error occurs the state will not be stuck on true
 except Exception as err:
     os.chdir("../")
     f = open("mcServerState.txt", "w")
@@ -67,7 +67,7 @@ except Exception as err:
     f.close()
 
     repo.git.add("mcServerState.txt")
-    repo.index.commit("World Save")
+    repo.index.commit("Server Failed Exception")
     origin = repo.remote(name='origin')
     origin.push()   
 
@@ -75,6 +75,7 @@ except Exception as err:
     print(err)
     exit()
 
+#Saves the world data and changes the state, also pushes to github
 f = open("mcServerState.txt", "w")
 f.write("False")
 f.close()
